00f9e0f933a6480c057cac62a60888a830513ced
James Pier <jpier2012@gmail.com>
2019-06-13 22:32:10 -0500

    add login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
   get "/signup" do
+    redirect "/workouts" if logged_in?
     erb :"users/signup"
   end
 
   post "/signup" do
-    redirect "/workouts" if logged_in?
     user = User.create(username: params[:username], password: params[:password])
     session[:user_id] = user.id
     redirect "/workouts"
+  end
+
+  get "/login" do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/workouts"
+    else
+      redirect "/login"
+    end
   end
 end
