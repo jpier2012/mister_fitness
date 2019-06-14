509ab0ea04baa5798d6abde4f93f8b8fa326eae3
James Pier <jpier2012@gmail.com>
2019-06-13 22:48:46 -0500

    stubbed workouts_controller routes and moved login/logout routes to sessions_controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,34 @@
 class WorkoutsController < ApplicationController
   get "/workouts" do
+    redirect_if_not_logged_in
+    #select all workouts for the current user and display them in a list format
     "Hello world"
   end
+
+  get "/workouts/new" do
+    redirect_if_not_logged_in
+    # erb :"workouts/new"
+  end
+
+  post "/workouts" do
+    # create new workout for current user
+  end
+
+  get "/workouts/:id" do
+    redirect_if_not_logged_in
+    # show details for specific workout
+    # erb :"workouts/show"
+  end
+
+  get "/workouts/:id/edit"
+    redirect_if_not_logged_in
+    # erb :"workouts/edit"
+  end
+
+  patch "/workouts/:id" do
+    # uses params[:id] to 
+  end
+
+  
+
 end
